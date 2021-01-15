99afab606d4bf3277f2cddead7691da844efff24
kenshirakawa <ken.shirakawa@tier4.jp>
2021-01-15 17:57:28 +0900

    add code for reprocudtbility

--- point_classification/2_learning/train_cluster/111_train_big_dataset_dual_point_cloud_all_info.py
+++ point_classification/2_learning/train_cluster/111_train_big_dataset_dual_point_cloud_all_info.py
@@ -34,6 +34,10 @@ session_conf = tf.ConfigProto(
     intra_op_parallelism_threads=1,
     inter_op_parallelism_threads=1
 )
+
+tf.set_random_seed(7)
+sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+K.set_session(sess)
  
 parser = argparse.ArgumentParser()
 parser.add_argument("--cluster", help="Runs script on cluster")
