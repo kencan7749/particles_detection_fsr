4ae33acdec425a92141f91cb8db241f7d1202e83
kenshirakawa <ken.shirakawa@tier4.jp>
2021-01-16 13:35:53 +0900

    add another cp

--- point_classification/2_learning/train_cluster/111_train_big_dataset_dual_point_cloud_all_info.py
+++ point_classification/2_learning/train_cluster/111_train_big_dataset_dual_point_cloud_all_info.py
@@ -238,15 +238,15 @@ for run in range(10):
             epochs = self.epochs
             samples = self.samples
             #batch_size = self.batch_size
-            sample = batch_size*(batch)
+            #sample = batch_size*(batch)
 
             # '\r' と end='' を使って改行しないようにする
             if self.last_val_acc and self.last_val_loss:
                 # val_acc/val_loss が表示可能
-                print("\rManual Epoch %d/%d (%d/%d) -- acc: %f loss: %f - val_acc: %f val_loss: %f" % (epoch+1, epochs, sample, samples, self.last_acc, self.last_loss, self.last_val_acc, self.last_val_loss), end='')
+                print("\rManual Epoch %d/%d (/%d) -- acc: %f loss: %f - val_acc: %f val_loss: %f" % (epoch+1, epochs, samples, self.last_acc, self.last_loss, self.last_val_acc, self.last_val_loss), end='')
             else:
                 # val_acc/val_loss が表示不可
-                print("\rManual Epoch %d/%d (%d/%d) -- acc: %f loss: %f" % (epoch+1, epochs, sample, samples, self.last_acc, self.last_loss), end='')
+                print("\rManual Epoch %d/%d (/%d) -- acc: %f loss: %f" % (epoch+1, epochs,  samples, self.last_acc, self.last_loss), end='')
 
         # batch開始時
         def on_batch_begin(self, batch, logs={}):
